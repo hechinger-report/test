554e37abb70deaee5fdc302c501d71b6240fd207
hechinger-report <damato@hechingerreport.org>
2018-12-23 02:52:00 -0500

    Production finished/Auth finished

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+
 const app = express();
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-});
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/', (req, res) => {
-	console.log(res)
-	res.send({hi:'there'})
-});
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 8080;
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// });
+
+// app.get('/', (req, res) => {
+// 	console.log(res)
+// 	res.send({hi:'there'})
+// });
+
+require('./routes/authRoutes')(app)
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
-
---
+++ services/passport.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+		.then(user => {
+			done(null, user)
+		});
+});
+
+passport.use(
+	new GoogleStrategy({
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback'
+		}, (accessToken, refreshToken, profile, done) => {
+			User.findOne({googleId: profile.id}).then(function(result) {
+				if (!result) {
+					new User({
+						googleId: profile.id
+					}).save().then((user) => {
+						done(null, user)
+					})
+					console.log("New user created")
+				} else {
+					done(null, result)
+				}
+				console.log("User logged in")
+			});			
+			// console.log('accessToken:', accessToken);
+			// console.log('refreshToken:', refreshToken);
+			// console.log('profile:', profile);			
+		})
+	);
