a728f33a66881e918faf843c309670a7ca148ffe
hechinger-report <damato@hechingerreport.org>
2018-12-23 03:40:23 -0500

    Heroku URI fix

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,7 @@ passport.use(
 	new GoogleStrategy({
 		clientID: keys.googleClientID,
 		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback',
+		callbackURL: 'https://boiling-plains-27909.herokuapp.com/auth/google/callback',
 		proxy: true
 		}, (accessToken, refreshToken, profile, done) => {
 			User.findOne({googleId: profile.id}).then(function(result) {
